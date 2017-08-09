c9574b5015616dc709de43b583a006a576d25709
sebas <sebastiasebas@gmail.com>
2017-08-10 00:47:40 +0200

    1.1.d - fem ping() des python

--- 1_gen_html.js
+++ 1_gen_html.js
@@ -6,7 +6,7 @@
 //    curl http://127.0.0.1:3001
 //    curl http://127.0.0.1:3001/ping
 //    curl http://127.0.0.1:3001/pagina.html
-//    http://192.168.1.123:3001/pagina.html     auto-reload every 90 seconds
+//    http://192.168.1.123:3001/pagina.html     auto-reload every 30 seconds
 //
 // GIT
 //
@@ -29,14 +29,28 @@
 // 1.1.a - start code
 // 1.1.b - delete old file, create new one
 // 1.1.c - llegir socis.json
+// 1.1.d - fer ping() des python
 //
 
-var myVersio     = "v1.1.c" ;
+var myVersio     = "v1.1.d" ;
 
-// var http    = require ( 'http' ) ;
-var express = require( 'express' ) ;
-var app     = express() ;
-const fs    = require( 'fs' ) ; // manage filesystem
+var express     = require( 'express' ) ;
+var app         = express() ;
+const fs        = require( 'fs' ) ; // manage filesystem
+var PythonShell = require( 'python-shell' ) ;
+
+var fitxer_socis = "socis.json" ;  // configuracio de socis i IPs
+var iNumSocis ;                    // numero actual de socis
+var dades_socis ;                  // guardem les dades 
+var idxSoci = 0 ;                  // soci amb el que estem treballant ara mateix
+
+var python_options = {
+    mode: 'text',
+    pythonPath: '/usr/bin/python',
+    pythonOptions: ['-u'],
+    scriptPath: '/home/pi/timer',
+    args: ['value1', 'value2']
+} ;
 
 var mData = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   
               ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
@@ -45,10 +59,11 @@ var mData = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
 // set some values in global var APP
 
 app.set( 'cfgPort', process.env.PORT || 3001 ) ;
-app.set( 'cfgLapse', 5900 ) ;
+app.set( 'cfgLapse_Gen_HTML', 180000 ) ; // mili-segons - gen HTML every ... 5 minuts = 300 segons, 3 minuts = 180 seg.
+app.set( 'cfgLapse_Do_Ping', 5000 ) ;    // mili-segons - do Ping every ... 5 seconds
 
 
-// set where do we server HTML pages from
+// set where do we serve HTML pages from
 
     app.use( '/', express.static(__dirname + '/public') ) ; // serve whatever is in the "public" folder at the URL <root>/:filename
 
@@ -83,7 +98,7 @@ Date.prototype.hhmmss = function () {
 
 function genTimeStamp ( arg ) {
 
-    var szOut = (new Date).yyyymmdd() + '-' + (new Date).hhmmss() + ' ' ;
+    var szOut = (new Date).yyyymmdd() + '-' + (new Date).hhmmss() ;
 //    console.log( 'gen a TimeStamp {' + szOut + '}' ) ;
     return szOut ;
 
@@ -92,12 +107,68 @@ function genTimeStamp ( arg ) {
 
 // lets implement what to do when the TIMEOUT lapse expires
 
-function myTimeoutFunction( arg ) {
+// (1) ping timeout
+
+function myTimeout_Do_Ping_Function ( arg ) { // ping a un soci
+
+    var iPing_IP = dades_socis [ idxSoci ].ip ;
+    var szOut = ">>> timeout Ping soci " + idxSoci + "/" + iNumSocis + ". " ;
+    szOut += "Stamp {" + genTimeStamp() + '}, IP {' + iPing_IP + '}, nom {' + dades_socis [ idxSoci ].user + '}' ;
+    console.log( szOut ) ;
+
+// fem ping() amb python
+
+    python_options.args[0] = iPing_IP ; // set IP to ping in python params
+
+    PythonShell.run( '2_do_ping.py', python_options, function( err, results ) {
+
+        if ( err ) throw err;
+        console.log( '(+) Python results (%j).', results ) ;
+// results is an array consisting of messages collected during execution
+
+// if "RC 0"  then "on", if "RC KO" then "off"
+
+        var ss_OK = "RC 0" ;
+        var idx = results.indexOf( ss_OK ) ; // "-1" means "not found"
+//        console.log( '(#) PING result (%j).', idx ) ;
+
+        if ( idx > 0 ) { // substring found
+
+            if ( dades_socis [ idxSoci ].status != '+' ) { // ip comes up
+                console.log( '(#) IP (%s) comes UP. Stamp (%s).', iPing_IP, (new Date).hhmmss() ) ;
+            } ;
+
+            dades_socis [ idxSoci ].status = '+' ; // set IP is UP
+
+        } else {
+
+            if ( dades_socis [ idxSoci ].status != '-' ) { // ip comes down right now
+                console.log( '(#) IP (%s) goes DOWN. Stamp (%s).', iPing_IP, (new Date).hhmmss() ) ;
+            } ;
+
+            dades_socis [ idxSoci ].status = '-' ; // set IP is DOWN
+
+        } ;
+
+    } ) ; // python shell
+
+
+// apuntem al soci seguent
+
+    idxSoci = idxSoci + 1 ;
+    if ( idxSoci >= iNumSocis ) { idxSoci = 0 ; } ;
+
+} ; // myTimeout_Do_Ping_Function()
+
+
+// (2) generate HTML timeout
+
+function myTimeout_Gen_HTML_Function ( arg ) { // generar pagina HTML
 
 // (a) update "mData" structure with some new data, as timestamp or song name
 
     var szNow = genTimeStamp() ;
-    console.log( "+++ got TIMEOUT. Stamp {" + szNow + '}' ) ;
+    console.log( ">>> timeout Generar HTML. Stamp {" + szNow + '}' ) ;
 
 // remove item at end
     var last = mData.pop() ; // remove from the end
@@ -115,7 +186,7 @@ function myTimeoutFunction( arg ) {
 // (b1) 
 
     var newFN = './public/pagina.nova' ;
-    console.log( '(b1) delete ' + newFN ) ;
+//    console.log( '(b1) delete ' + newFN ) ;
 
     fs.unlink( newFN, (err) => {
         if (err) {
@@ -131,7 +202,7 @@ function myTimeoutFunction( arg ) {
 // (b2)
 
         var newFN = './public/pagina.nova' ;
-        console.log( '(b2) create ' + newFN ) ;
+//        console.log( '(b2) create ' + newFN ) ;
 
         var S1 = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html40/loose.dtd">\n' ;
         S1 += '<HTML> <HEAD>\n' ;
@@ -141,12 +212,26 @@ function myTimeoutFunction( arg ) {
         S1 += '<TITLE>' + 'Its ' + (new Date).hhmmss() + '</TITLE>\n' ;
         S1 += '</HEAD>\n <BODY>\n' ;
 
-        var S2 = '<hr>\n' ;
-        mData.forEach( function( item, index, array ) {
-            S2 += '<p>' + index + ' | ' + item + '</p>\n' ;
-        } ) ; // forEach element of mData
+        var S2 = '<hr>\n <table class="t_socis">' ;
+        S2 += '<tr> <th> Nom <th> IP <th> Timestamp </tr>' ;
 
-        var S3 = '<hr>\n </BODY>\n </HTML>\n' ;
+//        mData.forEach( function( item, index, array ) {
+//            S2 += '<p>' + index + ' | ' + item + '</p>\n' ;
+//        } ) ; // forEach element of mData
+
+        var szTR = '.' ;
+
+        dades_socis.forEach( function ( soci, index ) {
+            if ( dades_socis [index].status == '+' ) {
+                szTR = '<tr class="t_soci_working">' ;
+            } else {
+                szTR = '<tr class="t_soci_aturat">' ;
+            } ;
+            S2 += szTR + '<td>' + dades_socis [index].user + '<td>' + dades_socis [index].ip + '<td>' + (new Date).hhmmss() + '</tr>\n' ;
+
+        } ) ; // forEach()
+
+        var S3 = '</table> <hr>\n </BODY>\n </HTML>\n' ;
 
         fs.writeFile( newFN, S1+S2+S3, (err) => {
             if (err) throw err;
@@ -154,7 +239,7 @@ function myTimeoutFunction( arg ) {
 
 // (b3)
             var oldFN = './public/pagina.html' ;
-            console.log( '(b3) delete ' + oldFN ) ;
+//            console.log( '(b3) delete ' + oldFN ) ;
             fs.unlink( oldFN, (err) => {
                 if (err) {
                     if ( err.code === 'ENOENT' ) {
@@ -169,7 +254,7 @@ function myTimeoutFunction( arg ) {
 // (b4)
                 var newFN = './public/pagina.html' ;
                 var oldFN = './public/pagina.nova' ;
-                console.log( '(b4) rename ' + oldFN + ' as ' + newFN ) ;
+//                console.log( '(b4) rename ' + oldFN + ' as ' + newFN ) ;
 
                 fs.rename( oldFN, newFN, (err) => {
                     if (err) throw err;
@@ -182,14 +267,37 @@ function myTimeoutFunction( arg ) {
 
     } ) ; // synch delete "pagina.nova"
 
-} ; // myTimeoutFunction()
+} ; // myTimeout_Gen_HTML_Function()
+
 
 // llegir les dades dels socis
 
-socis.json
+fs.readFile( fitxer_socis, 'utf8', function ( err, dadesJSON ) {
 
-// set a repetitive timeout
-setInterval( myTimeoutFunction, app.get( 'cfgLapse' ) ) ; // lets call own function every defined lapse
+    if ( err ) throw err ;
+
+    console.dir( "in" + dadesJSON ) ;
+    dades_socis = JSON.parse( dadesJSON ) ;
+    iNumSocis = dades_socis.length ; 
+
+    dades_socis.forEach( function ( soci, index ) {
+        console.log( "Index " + index + "/" + iNumSocis + " has" ) ;
+        console.log( "\tout.user   \t"   + dades_socis [index].user ) ;
+        console.log( "\tout.ip     \t"   + dades_socis [index].ip ) ;
+        console.log( "\tout.tf     \t"   + dades_socis [index].tf ) ;
+        console.log( "\tout.status \t"   + dades_socis [index].status ) ;
+    } ) ; // forEach()
+
+} ) ; // readFile()
+
+
+// set 2 repetitive timeouts
+
+//    console.log( "set TO html, timestamp" + (new Date).hhmmss() ) ;
+    setInterval( myTimeout_Gen_HTML_Function, app.get( 'cfgLapse_Gen_HTML' ) ) ; // lets call own function every defined lapse
+
+//    console.log( "set TO ping, timestamp" + (new Date).hhmmss() ) ;
+    setInterval( myTimeout_Do_Ping_Function, app.get( 'cfgLapse_Do_Ping' ) ) ;   //
 
 
 // implement branches answering the client requests
--- 2_do_ping.py
+++ 2_do_ping.py
@@ -4,6 +4,7 @@ import sys
 
 import pyping
 # pip install pyping
+# https://github.com/certator/pyping/blob/master/bin/pyping
 
 numArg = len( sys.argv )
 idx = 0
@@ -18,10 +19,10 @@ idx = 0
 szDesti = '74.125.143.104'
 szDesti = sys.argv[1]                         # in nodejs we code "python_options.args[0] = szIP ;"
 
-# print 'Lets ping (', szDesti, ').'
-response = pyping.ping( szDesti )
+print 'Lets ping (', szDesti, ').'
+response = pyping.ping( szDesti, timeout=900, count=2 )        # -t 900 -c 2
 
 if response.ret_code == 0:
     print( "RC 0 - Viagra, reachable" )            # 74.125.143.104
 else:
-    print( "RC else - Promescent, unreachable" )   # 1.2.3.4
+    print( "RC KO - Promescent, unreachable" )   # 1.2.3.4
